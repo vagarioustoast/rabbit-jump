347b4f2bf6a2761987ce1c613ceee7f331f55662
brandon <vagarioustoast@gmail.com>
2023-09-02 11:33:10 -0700

    adds platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,11 +10,28 @@ export default class Game extends Phaser.Scene
     preload()
     {
         this.load.image('background', 'assets/bg_layer1.png');
+        this.load.image('platform', 'assets/ground_grass.png')
     }
 
     create()
     {
-        this.add.image(240, 320, 'background')
+        this.add.image(240, 320, 'background');
+
+        const platforms = this.physics.add.staticGroup();
+
+        for (let i = 0; i < 5; ++i)
+        {
+            const x = Phaser.Math.Between(80, 400);
+            const y = 150 * i;
+
+            /** @type {Phaser.Physics.Arcade.Sprite */
+            const platform = platforms.create(x, y, 'platform');
+            platform.scale = 0.5;
+
+            /** @type {Phaser.Physics.Arcade.StaticBody} */
+            const body = platform.body;
+            body.updateFromGameObject();
+        }
     }
 }
 
